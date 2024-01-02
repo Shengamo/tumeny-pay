a7d86172569bb05d00b58a583e7ea137c1309b04
Mo Malenga <mo@shengamo.com>
2024-01-02 20:00:30 +0200

    repaired get Token error and verify payment get request

--- tests/Feature/TumenyTest.php
+++ tests/Feature/TumenyTest.php
@@ -6,12 +6,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
-//use Orchestra\Testbench\TestCase;
 use Shengamo\TumenyPay\Jobs\VerifyPendingOrderPayments;
 use Shengamo\TumenyPay\Models\Order;
 use Shengamo\TumenyPay\Models\ShengamoOrder;
 use Shengamo\TumenyPay\Tests\TestCase;
 use Shengamo\TumenyPay\TumenyPay;
+
 class TumenyTest extends TestCase
 {
     use RefreshDatabase;
@@ -63,32 +63,6 @@ class TumenyTest extends TestCase
 
         // Check that the token is stored in the cache
         $this->assertEquals('abcdef123456', Cache::get('tumeny_token'));
-    }
-
-    public function test_sending_payment_request()
-    {
-        $paymentId = "120022";
-        Http::fake([
-            config('tumeny.base_url') . 'v1/payment' => Http::response([
-                'payment' => [
-                    'id' => $paymentId,
-                    'amount' => 1,
-                    'status' => "PENDING",
-                    'message' => "PENDING",
-                    ],
-            ], 200)
-        ]);
-
-        Cache::put('tumeny_token', 'abcdef123456', Carbon::parse()->addHours(1)->timezone('UTC'));
-        $tumeny = new TumenyPay();
-
-        // Execute the method that generates and caches the token
-        $tumeny->processPayment(1, 'default','0968666077',1,'Payment for goods');
-
-//        Check to see if an order is created
-        $this->assertEquals(1, ShengamoOrder::count());
-        $this->assertEquals($paymentId, ShengamoOrder::first()->tx_ref);
-
     }
 
     public function test_payment_verified_as_successful()
