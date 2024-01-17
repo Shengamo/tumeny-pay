71a640ad6a967c8753d2a8948f50547b5640da76
Mo Malenga <mo@shengamo.com>
2024-01-17 10:02:15 +0200

    ignore if it exists already

--- database/migrations/create_team_subscriptions_table.php
+++ database/migrations/create_team_subscriptions_table.php
@@ -4,31 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
 
     public function up(): void
     {
-        Schema::create(
-            'team_subscriptions', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('team_id')->nullable();
-            $table->string('name');
-            $table->string('gateway_id')->nullable();
-            $table->string('gateway_name')->nullable();
-            $table->string('gateway_plan')->nullable();
-            $table->string('gateway_status')->nullable();
-            $table->integer('quantity')->default(1);
-            $table->bigInteger('amount');
-            $table->integer('app_fee')->default(0);
-            $table->string('tx_ref')->nullable();
-            $table->timestamp('trial_ends_at')->nullable();
-            $table->timestamp('ends_at')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
-            $table->tinyInteger('status')->nullable()->default(1);
+        if (!Schema::hasTable('team_subscriptions')) {
+            Schema::create(
+                'team_subscriptions', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('team_id')->nullable();
+                $table->string('name');
+                $table->string('gateway_id')->nullable();
+                $table->string('gateway_name')->nullable();
+                $table->string('gateway_plan')->nullable();
+                $table->string('gateway_status')->nullable();
+                $table->integer('quantity')->default(1);
+                $table->bigInteger('amount');
+                $table->integer('app_fee')->default(0);
+                $table->string('tx_ref')->nullable();
+                $table->timestamp('trial_ends_at')->nullable();
+                $table->timestamp('ends_at')->nullable();
+                $table->timestamps();
+                $table->softDeletes();
+                $table->tinyInteger('status')->nullable()->default(1);
             }
-        );
+            );
+        }
     }
 
     public function down(): void
